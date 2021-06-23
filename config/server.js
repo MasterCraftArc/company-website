92af0feafc9b079e58724f8c7a1afee13a345885
Wade Desir <wadedesir@Wades-MacBook-Pro.local>
2021-06-23 13:38:47 -0400

    closing #6 and closing #7 site now hosted at defenseunicorns.com

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const maintenance = require('nodejs-server-maintenance');
-maintenance(app);
+maintenance(app, true);
 
 let port = process.env.PORT
 if (port == null || port == ""){ 
