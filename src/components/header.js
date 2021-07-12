e00c107ad21b7341e585885344b20d4acd73266b
wededesir <wededesir@gmail.com>
2021-07-12 13:26:35 -0400

    updated components

--- src/components/header.js
+++ src/components/header.js
@@ -5,8 +5,8 @@ function Header(props) {
     return (
         <header className="mainHeader navbar navbar-expand{-sm|-md|-lg|-xl|-xxl}">
             <div className="container-fluid">
-            <Link to="/" className="navbar-brand"><img className="img-fluid" src="../images/logo.png"></img></Link>
-            <div>
+            <Link to="/" className="navbar-brand"><img className="img-fluid" src="../images/logo.png" alt="Defense Unicorns Logo"></img></Link>
+            <div className="border">
             <Link to="/"><button className="btn btn-primary">Train</button></Link>
             <Link to="/equip"><button className="btn btn-primary">Equip</button></Link>
             <Link to="/contact"><button className="btn btn-primary">Contact</button></Link>
