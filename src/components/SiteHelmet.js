85f1b2dc07a68e9c026773688696baeb6403a7b0
edengebrezgi <edengebrezgi@gmail.com>
2021-08-01 17:10:19 -0400

    updated footer styling for desktop

--- src/components/SiteHelmet.js
+++ src/components/SiteHelmet.js
@@ -13,6 +13,7 @@ function SiteHelmet(props) {
                 <meta name="viewport" content="width=device-width, initial-scale=1" /> 
                 {/* <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link> */}
                 {/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script> */}
+                <script src="https://kit.fontawesome.com/eb2da737c9.js" crossorigin="anonymous"></script>
             </Helmet>
         </div>
     );
