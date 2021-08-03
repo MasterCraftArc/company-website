c66eca847573cb0c75071cd879dc9cebe04ffd12
mike-winberry <mike@leapfrog.ai>
2021-08-03 15:42:30 -0600

    #41. MW. Updated the footer to be scalable on mobile.

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,45 +13,14 @@ const footerBox = {
   marginTop: "54vh",
 };
 
-const logoBox = {
-  // height: "50%",
-  // width: "100%",
-  // display: "flex",
-  // justifyContent: "front-end",
-  // alignItems: "baseline",
-};
-
 const logoStyle = {
   width: "37%",
-};
-
-const middleBox = {
-  // height: "35%",
-  // width: "100%",
-  // display: 'flex',
 };
 
 const socialsBox = {
   marginRight: "23%",
   paddingTop: "4%",
   paddingRight: "17%",
-  // marginLeft:'initial',
-  // height: "10%",
-  // width: "100%",
-  // color: 'white',
-  // fontSize: '20px',
-  // paddingLeft: '10%',
-};
-
-const addressBox = {
-  // height: "100%",
-  // width: "60%",
-  // color: 'white',
-  // lineHeight: '1',
-  // display: 'flex',
-  // flexDirection: 'column',
-  // justifyContent: 'center',
-  // paddingLeft: '10%',
 };
 
 const menuBox = {
@@ -72,21 +41,21 @@ const menu = {
 
 function Footer() {
   return (
-    <footer className="footer flex-end" style={footerStyle}>
+    <footer className="footer" style={footerStyle}>
       <div
-        className="container h-2/6 w-full flex flex-wrap mx-auto mr-7"
+        className="container md:h-2/6  w-full flex md:flex-row flex-col mx-auto mr-7"
         style={footerBox}
       >
-        <div className="flex flex-wrap w-2/5 xs:w-full">
+        <div className="flex flex-wrap md:w-2/5 xs:w-full">
           <div className="flex h-4/6 xs:w-full">
-            <div className="w-full xs:w-full" style={logoBox}>
+            <div className="w-full xs:w-full">
               <img
                 className="mx-auto mt-12 justify-self-start"
                 src={verticalLogo}
                 style={logoStyle}
               />
             </div>
-            <div className="flex w-3/4 flex-end xs:w-full" style={addressBox}>
+            <div className="flex w-3/4 flex-end xs:w-full">
               <div className="mx-auto text-2xl mt-16 text-white">
                 <i class="bi bi-house-door-fill text-red-600 inline-block"></i>
                 <p className="mt-2 inline-block ml-2">Defense Unicorns</p>
@@ -101,38 +70,35 @@ function Footer() {
             </p>
           </div>
         </div>
-
-        {/* <div className='border' style = {middleBox}> */}
-
-        <div
-          className="text-3xl border-black w-1/5 flex flex-col justify-center items-center pb-8 self-start text-white mx-auto mt-12"
-          style={menuBox}
-        >
-          <Link to="/train" className="footerLink" style={linkColor}>
-            <p style={menu}>Train</p>
-          </Link>
-          <Link to="/equip" className="footerLink" style={linkColor}>
-            <p style={menu}>Equip</p>
-          </Link>
-          <Link to="/contact" className="footerLink" style={linkColor}>
-            <p style={menu}>Contact</p>
-          </Link>
-        </div>
-        {/* </div> */}
-
-        <div style={socialsBox} className="socialsBox w-1/6 flex text-3xl">
-          <a className="mt-14 h-5 ml-12" href="#">
-            <i className="bi bi-twitter me-3 hover:bg-white hover:text-red-500 text-white rounded-full p-2 transition-all"></i>
-          </a>
-          <a
-            className="mt-14 h-5"
-            href="https://www.linkedin.com/company/leapfrog-ai/about/"
+        <div className="flex md:mx-12 mx-auto">
+          <div
+            className="text-3xl border-black w-1/5 flex flex-col justify-center items-center pb-8 self-start text-white mx-auto mt-12"
+            style={menuBox}
           >
-            <i className="bi bi-linkedin ms-1 me-3 hover:bg-white hover:text-red-500 text-white rounded-full p-2 transition-all"></i>
-          </a>
-          <a className="mt-14 h-5" href="#">
-            <i className="bi bi-facebook me-3 hover:bg-white hover:text-red-500 text-white rounded-full p-2 transition-all"></i>
-          </a>
+            <Link to="/train" className="footerLink" style={linkColor}>
+              <p style={menu}>Train</p>
+            </Link>
+            <Link to="/equip" className="footerLink" style={linkColor}>
+              <p style={menu}>Equip</p>
+            </Link>
+            <Link to="/contact" className="footerLink" style={linkColor}>
+              <p style={menu}>Contact</p>
+            </Link>
+          </div>
+          <div style={socialsBox} className="socialsBox w-1/6 flex text-3xl">
+            <a className="mt-14 h-5 ml-12" href="#">
+              <i className="bi bi-twitter me-3 hover:bg-white hover:text-red-500 text-white rounded-full p-2 transition-all"></i>
+            </a>
+            <a
+              className="mt-14 h-5"
+              href="https://www.linkedin.com/company/leapfrog-ai/about/"
+            >
+              <i className="bi bi-linkedin ms-1 me-3 hover:bg-white hover:text-red-500 text-white rounded-full p-2 transition-all"></i>
+            </a>
+            <a className="mt-14 h-5" href="#">
+              <i className="bi bi-facebook me-3 hover:bg-white hover:text-red-500 text-white rounded-full p-2 transition-all"></i>
+            </a>
+          </div>
         </div>
       </div>
     </footer>
