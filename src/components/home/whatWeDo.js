2906261a6673ce0dac0941424aa02bf2e142832e
mike-winberry <mike@leapfrog.ai>
2021-08-03 09:48:01 -0600

    MW. #41 updated copy in whatWeDo section

--- src/components/home/whatWeDo.js
+++ src/components/home/whatWeDo.js
@@ -2,7 +2,7 @@ import React from "react";
 import { TITLE_TEXT } from "./homeStyles";
 import whatWeDoPng from "../../images/caseStudies.png";
 import keyPoints from "../../images/keyPointsLight.png";
-import Button from "../button"
+import Button from "../button";
 
 const CaseStudies = () => {
   return (
@@ -14,9 +14,12 @@ const CaseStudies = () => {
         <div className="flex flex-col justify-center px-10">
           <h2 className={TITLE_TEXT}>What We Do</h2>
           <p className="pt-5 pb-5 lg:pt-10 lg:pb-32 lg:pr-10 xl:pr-8 text-3xl xl:pb-16 font-light">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            Lorem ipsum dolor sit amet.
+            We make it easier for mission-driven organizations to buy, accredit,
+            and integrate software solutions. We provide a secure, open source
+            and infrastructure agnostic DevSecOps platform while continuously
+            expanding the base of software product companies accessible to the
+            federal market to create an end-to-end solution for continuous
+            software integration.
           </p>
           <img
             className="md:w-full xl:w-1/2 py-5 pt-10"
@@ -25,11 +28,11 @@ const CaseStudies = () => {
           ></img>
 
           <div className="pt-7">
-            <Button 
-              linkTo='/equip' 
-              className='bg-transparent border-blue-900 border-2 border-solid hover:bg-blue-900 text-blue-900 hover:text-white' 
+            <Button
+              linkTo="/equip"
+              className="bg-transparent border-blue-900 border-2 border-solid hover:bg-blue-900 text-blue-900 hover:text-white"
               text="Learn More"
-              />
+            />
           </div>
         </div>
         <div className="p-16 pt-0 lg:block hidden object-cover">
