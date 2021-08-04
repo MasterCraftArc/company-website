16f06c5662bd2967b2ffc4ea0791c51e7315f84a
wededesir <wededesir@gmail.com>
2021-08-03 21:44:10 -0400

    fixed 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,11 +8,16 @@ const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
 
   return (
-    <Layout location={location} title={siteTitle}>
-      <Seo title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
+    // <Layout location={location} title={siteTitle}>
+    //   <Seo title="404: Not Found" />
+    //   <h1>404: Not Found</h1>
+    //   <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    // </Layout>
+    <div>
+      <h1>
+        404
+      </h1>
+    </div>
   )
 }
 
