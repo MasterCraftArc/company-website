22c4eaa12aecd5c30017b521dcb6ec1354af97fe
wededesir <wededesir@gmail.com>
2021-08-03 21:34:52 -0400

    removed test blog page

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,7 +1,23 @@
 var plugins = [{
+      name: 'gatsby-plugin-styled-components',
+      plugin: require('/Users/wadedesir/Documents/website/node_modules/gatsby-plugin-styled-components/gatsby-ssr'),
+      options: {"plugins":[],"displayName":true,"fileName":true,"minify":true,"namespace":"","transpileTemplateLiterals":true,"topLevelImportPaths":[],"pure":false},
+    },{
       name: 'gatsby-plugin-react-helmet',
-      plugin: require('/Users/eden/website/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/Users/wadedesir/Documents/website/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
+    },{
+      name: 'gatsby-plugin-image',
+      plugin: require('/Users/wadedesir/Documents/website/node_modules/gatsby-plugin-image/gatsby-ssr'),
+      options: {"plugins":[]},
+    },{
+      name: 'gatsby-plugin-feed',
+      plugin: require('/Users/wadedesir/Documents/website/node_modules/gatsby-plugin-feed/gatsby-ssr'),
+      options: {"plugins":[],"query":"\n          {\n            site {\n              siteMetadata {\n                title\n                description\n                siteUrl\n                site_url: siteUrl\n              }\n            }\n          }\n        ","feeds":[{"query":"\n              {\n                allMarkdownRemark(\n                  sort: { order: DESC, fields: [frontmatter___date] },\n                ) {\n                  nodes {\n                    excerpt\n                    html\n                    fields {\n                      slug\n                    }\n                    frontmatter {\n                      title\n                      date\n                    }\n                  }\n                }\n              }\n            ","output":"/rss.xml"}]},
+    },{
+      name: 'gatsby-plugin-manifest',
+      plugin: require('/Users/wadedesir/Documents/website/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      options: {"plugins":[],"name":"Gatsby Starter Blog","short_name":"GatsbyJS","start_url":"/","background_color":"#ffffff","theme_color":"#663399","display":"minimal-ui","icon":"src/images/gatsby-icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"4a9773549091c227cd2eb82ccd9c5e3a"},
     }]
 /* global plugins */
 // During bootstrap, we write requires at top of this file which looks like:
