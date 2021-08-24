f51e281244170bcb31127d5bf70ea88e6a4f9ab0
edengebrezgi <edengebrezgi@gmail.com>
2021-08-24 02:31:09 -0400

    updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,81 +9,75 @@ const footerStyle = {
   alignItems: "center",
   background: `url(${footerBackground}) no-repeat`,
   backgroundSize: "cover",
-  height: "60vh",
+  height: "70vh",
 };
 
 const logoStyle = {
-  width: "67%",
+  width: "135px",
+  left: '75%',
 
 };
 
 const socialsBox = {
-  marginRight: "23%",
-  paddingTop: "4%",
-  paddingRight: "17%",
+  // paddingTop: "4%",
+  // paddingRight: "17%",
+  color: 'white',
+  marginLeft: '-99%',
+  marginTop: '-18%',
 };
 
 const menuBox = {
-  marginLeft: "initial",
 };
 
 const linkColor = {
   fontSize: "14px",
-  fontWeight: "bold",
 };
 
 const menu = {
   display: "flex",
-  justifyContent: "right",
-  marginTop: "10px",
+  marginTop: '-11',
 };
+
+const addressBox = {
+  marginLeft: '-69%',
+  marginTop: '43%',
+}
+
+const copyRight = {
+  top: '81%',
+}
 
 function Footer({ background }) {
   const showBackground = background === undefined ? true : background;
 
   return (
     <footer
+      className='relative'
       style={{
         ...footerStyle,
         background: showBackground ? footerStyle.background : "",
       }}
-    >
-      <div className="container md:h-3/6 flex md:flex-row flex-col mr-7 mx-auto">
+      >
+        <img
+        className="bg-no-repeat bg-cover absolute bottom-11"
+        src={verticalLogo}
+        style={logoStyle}
+        alt="Defense Unicorns logo"
+      />
+      <div 
+        className="h-full md:h-3/6 flex md:flex-row flex-col mx-auto"
+      >
         <div className="flex flex-col md:w-2/5 xs:w-full">
           <div className="flex md:flex-row flex-col h-4/6 xs:w-full">
             
-            <div className="flex w-full mx-auto">
-              <div className="mx-auto md:text-left text-center text-2xl mt-16 text-white">
-                <i className="bi bi-house-door-fill text-red-600 inline-block"></i>
-                <p className="mt-2 inline-block ml-2">Defense Unicorns</p>
-                <p className="mt-2 ml-6">555 E Pikes Peak Ave</p>
-                <p className="mt-2 ml-6">Colorado Springs, Co 80903</p>
+            <div className="flex flex-col w-full mx-auto mt-2 ml-6">
+              <div className="md:text-left text-center text-xl mt-9 text-white"
+                    style={addressBox}>
+                <p className="mt-2">Defense Unicorns</p>
+                <p className="mt-2">555 E Pikes Peak Ave</p>
+                <p className="mt-2">Colorado Springs, Co 80903</p>
               </div>
-            </div>
-          </div>
-          <div className="w-full md:text-left text-center flex-grow h-2/6 text-gray-300 text-lg">
-            <p className="mt-12 md:ml-32">
-              Copyright © {new Date().getFullYear()} Defense Unicorns. All
-              rights reserved.
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-row md:mx-12 mx-auto">
-          <div
-            className="text-3xl border-black w-1/5 flex flex-col justify-center items-center self-start text-blue-900 mx-auto mt-12"
-            style={menuBox}
-          >
-            <Link to="/train" className="footerLink" style={linkColor}>
-              <p style={menu}>Train</p>
-            </Link>
-            <Link to="/equip" className="footerLink" style={linkColor}>
-              <p style={menu}>Equip</p>
-            </Link>
-            <Link to="/contact" className="footerLink" style={linkColor}>
-              <p style={menu}>Contact</p>
-            </Link>
-          </div>
-          <div style={socialsBox} className="socialsBox w-1/6 flex text-3xl">
+              <div style={socialsBox} className="socialsBox w-1/5 flex justify-items-center text-3xl">
             <button aria-hidden="true" className="mt-14 h-5 ml-12">
               <i className="bi bi-twitter me-3 hover:bg-white hover:text-red-500 text-white rounded-full p-2 transition-all"></i>
             </button>
@@ -98,16 +92,25 @@ function Footer({ background }) {
               <i className="bi bi-facebook me-3 hover:bg-white hover:text-red-500 text-white rounded-full p-2 transition-all"></i>
             </button>
           </div>
-          <div className="w-full">
-              <img
-                className="w-full bg-no-repeat bg-cover "
-                src={verticalLogo}
-                style={logoStyle}
-                alt="Defense Unicorns logo"
-              />
             </div>
+          </div>
+        </div>
+        <div className="flex flex-row md:mx-12 mx-auto text-white items-center text-2xl w-full justify-around top-15">
+              <Link to="/train" className="footerLink" style={linkColor}>
+                <p style={menu}>Train</p>
+              </Link>
+              <Link to="/equip" className="footerLink" style={linkColor}>
+                <p style={menu}>Equip</p>
+              </Link>
+              <Link to="/contact" className="footerLink" style={linkColor}>
+                <p style={menu}>Contact</p>
+              </Link>
         </div>
       </div>
+      <p className="mt-12 text-center absolute w-full text-white bottom-7">
+              Copyright © {new Date().getFullYear()} Defense Unicorns. All
+              rights reserved.
+            </p>
     </footer>
   );
 }
