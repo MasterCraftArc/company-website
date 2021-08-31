c156d69aa7cf8f28fd2d52aad6f1fe745343a6c4
Michael Winberry <mike@leapfrog.ai>
2021-08-31 05:34:29 -0600

    #112. Fix the nav bar on contact and training. Update the center image in home page. Compress equip hero, mobileHero, and postHero images

--- src/components/home/whoWeAre.js
+++ src/components/home/whoWeAre.js
@@ -1,7 +1,7 @@
 import React from "react";
 import whoWeAre from "../../images/home/whoWeAre.png";
 import Button from "../button";
-import whoBackground from "../../images/contact.png";
+import whoBackground from "../../images/contact.jpg";
 import pin from "../../images/locationPin.png";
 
 const WhoWeAre = () => {
