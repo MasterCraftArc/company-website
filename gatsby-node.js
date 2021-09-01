4c06076c9e65f9e7b5478a177549c5619a2e348f
wededesir <wededesir@gmail.com>
2021-08-31 21:39:25 -0400

    fixes #65

--- gatsby-node.js
+++ gatsby-node.js
@@ -20,6 +20,9 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
             fields {
               slug
             }
+            frontmatter {
+              published
+            }
           }
         }
       }
@@ -34,7 +37,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     return;
   }
 
-  const posts = result.data.allMarkdownRemark.nodes;
+  const posts = result.data.allMarkdownRemark.nodes.filter(post => post.frontmatter.published === true);
   // Create blog posts pages
   // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
   // `context` is available in the template as a prop and as a variable in GraphQL
