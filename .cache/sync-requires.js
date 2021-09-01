09df3de535b1d6cc16649c67f71836f6666ff260
Michael Winberry <mike@leapfrog.ai>
2021-09-01 07:31:16 -0600

    #65. Quick refactor example on gatsby-node for querying only published blog posts

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,15 +4,15 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/wadedesir/Documents/Github/website/.cache/dev-404-page.js")),
-  "component---src-pages-404-js": preferDefault(require("/Users/wadedesir/Documents/Github/website/src/pages/404.js")),
-  "component---src-pages-blogpost-js": preferDefault(require("/Users/wadedesir/Documents/Github/website/src/pages/blogpost.js")),
-  "component---src-pages-contact-js": preferDefault(require("/Users/wadedesir/Documents/Github/website/src/pages/contact.js")),
-  "component---src-pages-equip-js": preferDefault(require("/Users/wadedesir/Documents/Github/website/src/pages/equip.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/wadedesir/Documents/Github/website/src/pages/index.js")),
-  "component---src-pages-thankyou-js": preferDefault(require("/Users/wadedesir/Documents/Github/website/src/pages/thankyou.js")),
-  "component---src-pages-thankyoutraining-js": preferDefault(require("/Users/wadedesir/Documents/Github/website/src/pages/thankyoutraining.js")),
-  "component---src-pages-training-js": preferDefault(require("/Users/wadedesir/Documents/Github/website/src/pages/training.js")),
-  "component---src-templates-blog-post-js": preferDefault(require("/Users/wadedesir/Documents/Github/website/src/templates/blog-post.js"))
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/mikewinberry/leapfrog/website/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/mikewinberry/leapfrog/website/src/pages/404.js")),
+  "component---src-pages-blogpost-js": preferDefault(require("/Users/mikewinberry/leapfrog/website/src/pages/blogpost.js")),
+  "component---src-pages-contact-js": preferDefault(require("/Users/mikewinberry/leapfrog/website/src/pages/contact.js")),
+  "component---src-pages-equip-js": preferDefault(require("/Users/mikewinberry/leapfrog/website/src/pages/equip.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/mikewinberry/leapfrog/website/src/pages/index.js")),
+  "component---src-pages-thankyou-js": preferDefault(require("/Users/mikewinberry/leapfrog/website/src/pages/thankyou.js")),
+  "component---src-pages-thankyoutraining-js": preferDefault(require("/Users/mikewinberry/leapfrog/website/src/pages/thankyoutraining.js")),
+  "component---src-pages-training-js": preferDefault(require("/Users/mikewinberry/leapfrog/website/src/pages/training.js")),
+  "component---src-templates-blog-post-js": preferDefault(require("/Users/mikewinberry/leapfrog/website/src/templates/blog-post.js"))
 }
 
--- gatsby-node.js
+++ gatsby-node.js
@@ -12,6 +12,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     `
       {
         allMarkdownRemark(
+          filter: { frontmatter: { published: { $eq: true } } }
           sort: { fields: [frontmatter___date], order: ASC }
           limit: 1000
         ) {
@@ -19,9 +20,6 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
             id
             fields {
               slug
-            }
-            frontmatter {
-              published
             }
           }
         }
@@ -37,28 +35,25 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     return;
   }
 
-  const posts = result.data.allMarkdownRemark.nodes.filter(post => post.frontmatter.published === true);
+  const posts = result.data.allMarkdownRemark.nodes;
   // Create blog posts pages
   // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
   // `context` is available in the template as a prop and as a variable in GraphQL
 
-  if (posts.length > 0) {
-    posts.forEach((post, index) => {
-      const previousPostId = index === 0 ? null : posts[index - 1].id;
-      const nextPostId =
-        index === posts.length - 1 ? null : posts[index + 1].id;
+  posts.forEach((post, index) => {
+    const previousPostId = index === 0 ? null : posts[index - 1].id;
+    const nextPostId = index === posts.length - 1 ? null : posts[index + 1].id;
 
-      createPage({
-        path: `/blog${post.fields.slug}`,
-        component: blogPost,
-        context: {
-          id: post.id,
-          previousPostId,
-          nextPostId,
-        },
-      });
+    createPage({
+      path: `/blog${post.fields.slug}`,
+      component: blogPost,
+      context: {
+        id: post.id,
+        previousPostId,
+        nextPostId,
+      },
     });
-  }
+  });
 };
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
