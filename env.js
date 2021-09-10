56c8b5d85b5bc8c79ab74f65d599332b9aea0358
Michael Winberry <mike@leapfrog.ai>
2021-09-09 21:09:59 -0600

    #173 add google tag manager. Configure initial universal behavior

--- env.js
+++ env.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
 module.exports = {
-  G_UNIVERSAL: process.env.G_UNIVERSAL || "development",
-  G_4_TAG: process.env.G_4_TAG || "development",
+  TAG_MGR_ID: process.env.TAG_MGR_ID || "development",
   SITEMAP_URL: process.env.SITEMAP_URL || "https://du-develop.com",
 };
