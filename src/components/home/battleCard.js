d453f6a401cd48a3d6d0cd1af73e3040fda52c7f
Michael Winberry <mike@leapfrog.ai>
2021-09-15 10:26:04 -0600

    Test, wrapping battlecard in link to capture url value

--- src/components/home/battleCard.js
+++ src/components/home/battleCard.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { navigate } from "gatsby";
 
 const cardStyle = {
   minWidth: "300px",
@@ -20,38 +19,31 @@ const cardImg = {
 
 const BattleCard = (props) => {
   return (
-    <div
-      className="flex mx-2 battleCard"
-      aria-hidden="true"
-      onClick={() => {
-        navigate(props.cardLink);
-      }}
-      onKeyDown={() => {
-        navigate(props.cardLink);
-      }}
-    >
-      <div
-        className="card
-          flex flex-col justify-evenly mx-auto p-12"
-        style={cardStyle}
-      >
-        <div className="flex justify-center items-center w-full">
-          <img
-            src={props.image}
-            style={cardImg}
-            className="card-img-top img-fluid"
-            alt={props.altText}
-          />
-        </div>
+    <a class=".battleCardLink" href={props.cardLink}>
+      <div className="flex mx-2 battleCard" aria-hidden="true">
+        <div
+          className="card
+        flex flex-col justify-evenly mx-auto p-12"
+          style={cardStyle}
+        >
+          <div className="flex justify-center items-center w-full">
+            <img
+              src={props.image}
+              style={cardImg}
+              className="card-img-top img-fluid"
+              alt={props.altText}
+            />
+          </div>
 
-        <div className="card-body text-center text-blue-900">
-          <h3 className="text-center mt-8 text-3xl">{props.heading}</h3>
-          <h5 className="card-title mt-3 mb-5 font-bold" style={cardTitle}>
-            {props.mainText}
-          </h5>
+          <div className="card-body text-center text-blue-900">
+            <h3 className="text-center mt-8 text-3xl">{props.heading}</h3>
+            <h5 className="card-title mt-3 mb-5 font-bold" style={cardTitle}>
+              {props.mainText}
+            </h5>
+          </div>
         </div>
       </div>
-    </div>
+    </a>
   );
 };
 
