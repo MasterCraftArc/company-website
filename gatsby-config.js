e768a4383e5495fd75a30c0863dd7710c797f6b5
wededesir <wededesir@gmail.com>
2022-01-18 14:43:03 -0500

    updated favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -125,7 +125,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/unicornSingle.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
       },
     },
     {
