70124b94d7dac13c8fe7fe0ca1c80e38ea34c134
edengebrezgi <edengebrezgi@gmail.com>
2022-03-25 23:29:16 -0400

    margin top of infinity image on home page

--- src/components/home/whatWeDo.js
+++ src/components/home/whatWeDo.js
@@ -6,10 +6,10 @@ import whoWeAre from "../../images/home/whoWeAre.png";
 
 const WhatWeDo = () => {
   return (
-    <section className="flex flex-col items-center justify-center w-full md:my-0 relative mt-10">
+    <section className="flex flex-col items-center justify-center w-full md:my-0 relative mt-30">
 
 <div className="xl:w-4/5 mx-auto grid lg:grid-cols-2 grid-cols-1">
-        <div className="w-5/6 md:w-4/6 lg:w-full mx-auto px-16 md:p-16 relative">
+        <div className="w-5/6 md:w-4/6 lg:w-full mx-auto px-16 md:p-16 relative pt-16 mt-16">
           <div className="tk-blob">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 274 303.2">
               <path d="M260.2 41.4c20 29.2 14.6 74.5 7.2 124.4-7.3 49.9-16.6 104.5-49.2 126-32.5 21.6-88.4 10.2-132-15.2s-75-64.7-83.6-107.8C-6.1 125.7 8 79 36.3 47.8 64.5 16.7 107 1.3 150.9.1c43.9-1.1 89.3 12 109.3 41.3z"></path>
@@ -22,7 +22,7 @@ const WhatWeDo = () => {
             alt="unicorn under magnifying glass"
           />
         </div>
-        <div className="flex flex-col justify-center px-8 md:px-20">
+        <div className="flex flex-col justify-center px-8 py-16 md:px-20">
           <h2
             className={`homeSectionTitle text-3xl sm:text-4xl md:text-5xl xl:text-6xl font-medium flex items-center text-blue-900`}
           >
@@ -37,7 +37,7 @@ const WhatWeDo = () => {
             digital warriors inside and outside the government - enabling their
             transformation from unicorn to hero.
           </p>
-          <div className="flex justify-center w-full sm:block">
+          <div className="flex justify-center w-full sm:block pt-7 pb-10 sm:pb-7">
             <Button
               linkTo="/about"
               className="bg-red-500 hover:bg-red-700 text-white mb-5 px-4"
